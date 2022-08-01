491ff64d5fa22320dcc794ae3f413337f2b3f382
KWAKTAEWOOK <rhkr6942@naver.com>
2022-08-01 20:39:34 +0900

    할일 등록

--- src/App.js
+++ src/App.js
@@ -5,17 +5,41 @@ function App() {
   const onChange = (e) => {
     setTodo(e.target.value);
   };
+  const onSubmit = (e)=> {
+    e.preventDefault();
+    if(todo === ""){
+      alert("한글자 이상 입력해주세요")
+      return;//유효성 검사
+    }
+    setTodos((currentArray) => [todo, ...currentArray]);
+    setTodo("");
+    alert("등록됨")
+  };
   return (
     <div>
       <h1>할 일</h1>
-      <form action="">
+      <form onSubmit={onSubmit}>
         <input
           type="text"
           onChange={onChange}
           value={todo}
           placeholder="할 일을 적어주세요."
         />
+        <button>등록</button>
       </form>
+      <button 
+        onClick={()=>{
+          console.log(todos);
+        }}
+      >
+        체크
+      </button>
+      <hr />
+      <ul>
+          {todos.map((todo, index)=> (
+            <li key={index}>{todo}</li>
+          ))}
+      </ul>
     </div>
   );
 }
