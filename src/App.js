1a770facea47534238dc8b884ce8ffdd8aebcd0b
KWAKTAEWOOK <rhkr6942@naver.com>
2022-08-03 16:27:42 +0900

    수정 팝업창만듬

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState,useRef } from "react";
+import TodoEdit from "./components/TodoEdit";
 import TodoInsert from "./components/TodoInsert";
 import TodoList from "./components/TodoList";
 import TodoTemplate from "./components/TodoTemplate";
 
 function App() {
   const [todos, setTodos] = useState([]);
+  const [insertToggle, setInsertToggle] = useState(false);
   const nextId= useRef(1);
   const onInsert = (text) => {
     const todo = {//할일을 불러와야한다.
@@ -15,6 +17,9 @@ function App() {
     };
     setTodos((todos)=> todos.concat(todo));//(추가로직)todos는 빈배열이고 온인써트를입력받으면 todo에 값을 받아와서 todos에 담아준다.
     nextId.current++;//current는 현재값을 받아옴++는1씩증가
+  };
+  const onInsertToggle = () => {
+    setInsertToggle((prev) => !prev);
   };
 
   const onRemove = id => {
@@ -33,7 +38,8 @@ function App() {
   return (
   <TodoTemplate> 
     <TodoInsert onInsert={onInsert} />  
-    <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} /> 
+    <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} onInsertToggle={onInsertToggle} /> 
+    {insertToggle && <TodoEdit onInsertToggle={onInsertToggle} />}
   </TodoTemplate>// TodoTemplate.jsx 안에 app.title,content 출력
 
 )}
--- src/components/Todolist.jsx
+++ src/components/Todolist.jsx
@@ -3,11 +3,11 @@ import TodoListItem from './TodoListItem';
 
 
 
-const TodoList = ({todos, onToggle , onRemove}) => {
+const TodoList = ({ todos, onRemove, onToggle, onInsertToggle }) => {
     return (
         <div className="TodoList">
             {todos.map((todo, index) => (// 온 토글이 눌리면 텍스트가 렌더링되는로직
-                <TodoListItem todo={todo} key={index} onToggle={onToggle} onRemove={onRemove} />
+                <TodoListItem todo={todo} key={index} onToggle={onToggle} onRemove={onRemove} onInsertToggle={onInsertToggle}/>
             ))}
         </div>
     );
